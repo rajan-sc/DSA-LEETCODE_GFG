12d88647efd35c4e95f5c6fa6247f7dd705b2610
Rajan Singh <164380069+rajan-sc@users.noreply.github.com>
2026-07-13 15:38:24 +0530

    Time: 0 ms (100%), Space: 19.5 MB (21.4%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -5,16 +5,17 @@ class Solution:
         """
         n = len(nums)
         s = 0
-        e = n - 1
         m = 0
+        e = n - 1
         while m <= e:
             if nums[m] == 0:
-                nums[m], nums[s] = nums[s], nums[m]
-                m += 1
+                nums[s], nums[m] = nums[m], nums[s]
                 s += 1
+                m += 1
             elif nums[m] == 1:
                 m += 1
-            else:
-                nums[m], nums[e] = nums[e], nums[m]
+            elif nums[m] == 2:
+                nums[e], nums[m] = nums[m], nums[e]
                 e -= 1
-            
+
+        
